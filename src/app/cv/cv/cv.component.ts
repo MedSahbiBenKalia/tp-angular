c84b3e3e9558f41e7c7d0ffa1b01520458482e3d
MedSahbiBenKalia <mohamedsahbi.benkalia@insat.ucar.tn>
2025-11-28 01:53:40 +0100

    rxResource in cv and detail-cv

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -7,7 +7,8 @@ import { ListComponent } from "../list/list.component";
 import { CvCardComponent } from "../cv-card/cv-card.component";
 import { EmbaucheComponent } from "../embauche/embauche.component";
 import { UpperCasePipe, DatePipe } from "@angular/common";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { rxResource, takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { catchError, of } from "rxjs";
 @Component({
     selector: "app-cv",
     templateUrl: "./cv.component.html",
@@ -26,23 +27,40 @@ export class CvComponent {
   private toastr = inject(ToastrService);
   private cvService = inject(CvService);
 
-  cvs: Cv[] = [];
+
   selectedCv: Cv | null = null;
   /*   selectedCv: Cv | null = null; */
   date = new Date();
 
+  cvResouce = rxResource({
+    loader: () => this.cvService.getCvs().pipe(
+      catchError(
+        () => {
+          this.toastr.error(`
+            Attention!! Les données sont fictives, problème avec le serveur.
+            Veuillez contacter l'admin.`);
+          
+          return of(this.cvService.getFakeCvs());
+        }
+
+      )
+    ),
+  })
+
+  cvs = this.cvResouce.value;
+
   constructor() {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
-        this.toastr.error(`
-          Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`);
-      },
-    });
+    // this.cvService.getCvs().subscribe({
+    //   next: (cvs) => {
+    //     this.cvs = cvs;
+    //   },
+    //   error: () => {
+    //     this.cvs = this.cvService.getFakeCvs();
+    //     this.toastr.error(`
+    //       Attention!! Les données sont fictives, problème avec le serveur.
+    //       Veuillez contacter l'admin.`);
+    //   },
+    // });
     this.logger.logger("je suis le cvComponent");
     this.toastr.info("Bienvenu dans notre CvTech");
     this.cvService.selectCv$
